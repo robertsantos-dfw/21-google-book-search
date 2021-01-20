775acb7a64f5491d9466e47e7e24350131219ad4
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-19 23:13:19 -0600

    modifying book.js and added an api.js file with .env to hide key

--- models/book.js
+++ models/book.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model("GoogleBooks", bookSchema);
 
 module.exports = Book;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
-const routes = require("./routes");
 const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +18,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+  
