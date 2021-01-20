3afc561c2a61519e7e2db4a83f781d62973ee5d8
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-19 18:45:40 -0600

    added pages for detail, nomatch, book

---
+++ client/src/pages/Books.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Jumbotron from "../components/Jumbotron";
+import DeleteBtn from "../components/DeleteBtn";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+import API from "../utils/API";
+
+function Books() {
+  // Initialize books as an empty array
+    const [books, setBooks] = useState([]);
+  
+    useEffect(() => {
+      loadBooks();
+    }, []);
+
+    function loadBooks() {
+      // Add code here to get all books from the database and store them using setBooks
+        API.getBooks()
+          .then(res => setBooks(res.data))
+          .catch(err => console.log(err));
+      
+    }
+
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>What Books Should I Read?</h1>
+            </Jumbotron>
+            <form>
+              <Input onChange={() =>{}} name="title" placeholder="Title (required)" />
+              <Input onChange={() =>{}} name="author" placeholder="Author (required)" />
+              <TextArea onChange={() =>{}} name="synopsis" placeholder="Synopsis (Optional)" />
+              <FormBtn>Submit Book</FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Books On My List</h1>
+            </Jumbotron>
+            {books.length ? (
+              <List>
+                {books.map((book) => (
+                  <ListItem key={book._id}>
+                    <a href={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </a>
+                    <DeleteBtn />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
+export default Books;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const routes = require("./routes");
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
