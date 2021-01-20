a06b73b04745b3be486c03ca47e143b1724bc570
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-20 00:18:34 -0600

    updating route to save

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,15 @@
+  
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 // Matches with "/api/books"
 router.route("/")
   .get(booksController.findAll)
-  .post(booksController.create);
+  .post(booksController.save);
 
 // Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
+router.route("/:id")
+  .get(booksController.findOne)
   .delete(booksController.remove);
 
 module.exports = router;
