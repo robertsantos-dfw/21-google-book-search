d4602aecec1c4413b2c21dbacbf88e3559d5641f
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-19 23:21:28 -0600

    added additional fields to schema

--- models/book.js
+++ models/book.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  link: { type: String, required: true }
+  id: {type: String, require: true},
+  title: { type: String, require: true},
+  authors: [{ type: String, require: true}],
+  description: { type: String, require: true},
+  image: { type: String, unique: true, dropDups: true},
+  link: { type: String, required: true}
 });
 
-const Book = mongoose.model("GoogleBooks", bookSchema);
+const GoogleBook = mongoose.model("GoogleBooks", bookSchema);
 
-module.exports = Book;
+module.exports = GoogleBook;
