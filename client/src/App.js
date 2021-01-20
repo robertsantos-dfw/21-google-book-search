e8f7a3c8bc6781741efce2b8419b3ff2e2f52c31
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-20 00:15:17 -0600

    updated controller info and style.css for APP file

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
-import "./App.css";
+//import "./App.css";
 
 
 
