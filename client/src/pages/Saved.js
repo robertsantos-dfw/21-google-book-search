3a41ef7c056360038375547e2e3ffe98bd9be2b2
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-19 23:35:50 -0600

    updated filename to Saved and have CRUD for saving books

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { Container } from "../components/Grid/Grid";
+import Nav from "../components/Nav/Nav";
+import Jumbotron from "../components/Jumbotron/Jumbotron";
+import API from '../utils/API';
+import SavedList from "../components/SavedList/SavedList";
+
+class Saved extends Component {
+
+    state = {
+        savedBooks: []
+    }
+
+    componentDidMount = () => {
+        this.getBooks()
+    }
+
+    deleteGoogleBook = currentBook => {
+        API.deleteBook( currentBook.id )
+        .then(res => {
+            console.log("You deleted this book:", res);
+            this.getBooks();
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
+    getBooks = () => {
+        API.getBooks()
+        .then(res => {
+            this.setState({
+                savedBooks: res.data
+            })
+            console.log("This is the res from getBooks", res);
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Container fluid>
+                <Jumbotron />
+                {this.state.savedBooks.length ? (
+                    <SavedList 
+                    bookState={this.state.savedBooks}
+                    deleteGoogleBook={this.deleteGoogleBook}
+                    >
+                    </SavedList>
+                ) : (
+                    <h5>No results to display</h5>
+                )}
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Saved
