f487966dc58c1b8a1e30270981112c886ffd6902
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-19 23:29:58 -0600

    updated axios get  query

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+    // Retrieves Google books
+    googleBooks: function(query) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    },
+    // Saves book to the DB
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+    // Gets saved books from DB
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    // Gets book with the given ID
+    getBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+    // Deletes book with the given ID
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
 };
