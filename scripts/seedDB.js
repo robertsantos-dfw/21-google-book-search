f94740cee5a7d17eca6eb446a916e4add67b75d7
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-20 14:01:49 -0600

    updating route

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,16 +5,17 @@ const db = require("../models");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+  "mongodb://localhost/GoogleBooks"
 );
 
 const bookSeed = [
   {
-    title: "The Dead Zone",
-    author: "Stephen King",
-    synopsis:
-      "A number-one national best seller about a man who wakes up from a five-year coma able to see people's futures and the terrible fate awaiting mankind in The Dead Zone - a \"compulsive page-turner\" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people's futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a \"faultlessly paced...continuously engrossing\" (Los Angeles Times) novel of second sight.",
-    date: new Date(Date.now())
+  id: "1",
+  title: "Lord of the Flies",
+  authors: ["William Golding"],
+  description: "The tale of a party of shipwrecked schoolboys, marooned on a coral island, who at first enjoy the freedom of the situation but soon divide into fearsome gangs which turn the paradise island into a nightmare of panic and death.",
+  image: "http://books.google.com/books/content?id=r6eoCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  link: "http://books.google.com/books?id=r6eoCwAAQBAJ&printsec=frontcover&dq=lord+of+the+flies&hl=&cd=1&source=gbs_api"
   },
   {
     title: "Lord of the Flies",
