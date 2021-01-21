e321a0ce0e92f20035485d62d6b7526d9e44a28d
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-21 11:59:20 -0600

    updated schema require from required

--- models/book.js
+++ models/book.js
@@ -7,7 +7,7 @@ const bookSchema = new Schema({
   authors: [{ type: String, require: true}],
   description: { type: String, require: true},
   image: { type: String, unique: true, dropDups: true},
-  link: { type: String, required: true}
+  link: { type: String, require: true}
 });
 
 const GoogleBooks = mongoose.model("GoogleBooks", bookSchema);
